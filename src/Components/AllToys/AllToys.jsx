fb8ba119a50917a877eecc4f3404e01a736e04da
mursalin@gmail.com <MURSA>
2023-05-19 21:26:52 +0600

    updated the all toy componnet for showing 20 items defult

--- src/Components/AllToys/AllToys.jsx
+++ src/Components/AllToys/AllToys.jsx
@@ -7,30 +7,9 @@ import { FaRegTrashAlt } from "react-icons/fa";
 const AllToys = () => {
   const loadedUsers = useLoaderData();
   const [users, setUsers] = useState(loadedUsers);
+  const [showAll, setShowAll] = useState(false);
 
-  const handleDelete = (_id) => {
-    Swal.fire({
-      title: "Are you sure?",
-      text: "You won't be able to revert this!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        fetch(`http://localhost:5000/users/${_id}`, { method: "DELETE" })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.deletedCount > 0) {
-              const remaining = users.filter((user) => user._id !== _id);
-              setUsers(remaining);
-              Swal.fire("Deleted!", "Your file has been deleted.", "success");
-            }
-          });
-      }
-    });
-  };
+  const DEFAULT_ITEMS_COUNT = 20;
 
   return (
     <div className="container mx-auto">
@@ -62,39 +41,54 @@ const AllToys = () => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
-              <tr key={user._id}>
-                <td className="px-4 py-3 border-b border-r">
-                  <img
-                    className="w-12 h-12 mx-auto rounded-md"
-                    src={user.img}
-                    alt=""
-                  />
-                </td>
-                <td className="px-4 py-3 border-b border-r">
-                  {user.sellerName}
-                </td>
-                <td className="px-4 py-3 border-b border-r">{user.name}</td>
-                <td className="px-4 py-3 border-b border-r">
-                  {user.subCategory}
-                </td>
-                <td className="px-4 py-3 border-b border-r">{user.price}</td>
-                <td className="px-4 py-3 border-b border-r">{user.quantity}</td>
-                <td className="px-4 py-3 border-b">
-                  <div className="flex justify-center">
-                    <Link to={`/details/${user._id}`}>
-                      <button className="text-white font-bold bg-blue-950 p-2 md:p-3 hover:bg-blue-800 rounded-full">
-                        View Details
-                      </button>
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
+            {users.map((user, index) => {
+              if (!showAll && index >= DEFAULT_ITEMS_COUNT) {
+                return null; // Skip rendering if not showing all and index is greater than or equal to the default items count
+              }
+              return (
+                <tr key={user._id}>
+                  <td className="px-4 py-3 border-b border-r">
+                    <img
+                      className="w-12 h-12 mx-auto rounded-md"
+                      src={user.img}
+                      alt=""
+                    />
+                  </td>
+                  <td className="px-4 py-3 border-b border-r">
+                    {user.sellerName}
+                  </td>
+                  <td className="px-4 py-3 border-b border-r">{user.name}</td>
+                  <td className="px-4 py-3 border-b border-r">
+                    {user.subCategory}
+                  </td>
+                  <td className="px-4 py-3 border-b border-r">{user.price}</td>
+                  <td className="px-4 py-3 border-b border-r">
+                    {user.quantity}
+                  </td>
+                  <td className="px-4 py-3 border-b">
+                    <div className="flex justify-center">
+                      <Link to={`/details/${user._id}`}>
+                        <button className="text-white font-bold bg-blue-950 p-2 md:p-3 hover:bg-blue-800 rounded-full">
+                          View Details
+                        </button>
+                      </Link>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
       <div className="text-center mb-5">
+        {!showAll && (
+          <button
+            className="bg-blue-950 text-white py-2 px-4 rounded mt-6"
+            onClick={() => setShowAll(true)}
+          >
+            See All
+          </button>
+        )}
         <Link to="/addtoys">
           <button className="bg-blue-950 text-white py-2 px-4 rounded mt-6">
             Add a Toy
